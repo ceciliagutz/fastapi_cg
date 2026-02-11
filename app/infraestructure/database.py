56cacb4a712e541e3ebadcdc527916185627b7fc
CeciliaGutierrez <ceciliagutierrezurbano@gmail.com>
2026-02-11 02:17:09 -0600

    feat: add PostgreSQL connection configuration

--- app/infraestructure/database.py
+++ app/infraestructure/database.py
@@ -1,26 +1,22 @@
-#Project Cecilia Gutierrez
+#Project CG
 
+import os
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("Database_url", "postgresql+asyncpg://user:password@localhost/dbname")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_async_engine(DATABASE_URL, echo=True, future=True)
+engine = create_async_engine(DATABASE_URL, echo=True)
 
 AsyncSessionLocal = sessionmaker(
-    bind = engine, 
-    expire_on_commit=False,
-    class_=AsyncSession
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False
 )
-
-Base= declarative_base()
 
 async def get_db():
     async with AsyncSessionLocal() as session:
         yield session
-                         
